aaf56c0740ff7513f7b6c0e1943479e670ee8b24
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-10 06:43:12 +0100

    fixed linter errors

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 const menubar = document.querySelector('.menu-bars');
 const navflex = document.querySelector('#desktop-nav');
 
-
 const mobilemenu = () => {
-    menubar.classList.toggle('toggle-menu');
-    navflex.classList.toggle('toggle');
-  };
+  menubar.classList.toggle('toggle-menu');
+  navflex.classList.toggle('toggle');
+};
 
-  menubar.addEventListener('click', mobilemenu);
-  
-
-  
+menubar.addEventListener('click', mobilemenu);
