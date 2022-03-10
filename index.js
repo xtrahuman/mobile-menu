045b46891cd0ef3f7217e417eda3e2be1bac4134
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-10 12:17:31 +0100

    Fixed mobile menu closing issue

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 // Display mobile menu
 const menubar = document.querySelector('.menu-bars');
 const navflex = document.querySelector('#desktop-nav');
+const bodyfix = document.querySelector('body');
+
+
+window.onclick = function check(event) {
+    if (
+      event.target.matches('.nav-link')
+        // || event.target.matches('.check')
+        // || event.target.matches('.fa-angle-right')
+    ) {
+      navflex.classList.remove('toggle');
+      menubar.classList.remove('toggle-menu');
+      bodyfix.classList.remove('static');
+    }
+  };
 
 const mobilemenu = () => {
   menubar.classList.toggle('toggle-menu');
   navflex.classList.toggle('toggle');
+  bodyfix.classList.remove('static');
 };
 
 menubar.addEventListener('click', mobilemenu);
