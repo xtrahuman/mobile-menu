8f8bec79200555b0f77fe8f19d88c7061e8a59c0
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-10 12:19:09 +0100

    fixed linter errors

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const menubar = document.querySelector('.menu-bars');
 const navflex = document.querySelector('#desktop-nav');
 const bodyfix = document.querySelector('body');
 
-
 window.onclick = function check(event) {
-    if (
-      event.target.matches('.nav-link')
-        // || event.target.matches('.check')
-        // || event.target.matches('.fa-angle-right')
-    ) {
-      navflex.classList.remove('toggle');
-      menubar.classList.remove('toggle-menu');
-      bodyfix.classList.remove('static');
-    }
-  };
+  if (
+    event.target.matches('.nav-link')
+  // || event.target.matches('.check')
+  // || event.target.matches('.fa-angle-right')
+  ) {
+    navflex.classList.remove('toggle');
+    menubar.classList.remove('toggle-menu');
+    bodyfix.classList.remove('static');
+  }
+};
 
 const mobilemenu = () => {
   menubar.classList.toggle('toggle-menu');
